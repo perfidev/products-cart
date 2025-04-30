6636e4ad6bed01c41603252ef686f07baebfb914
Raphael Perfi <perfi.dev@proton.me>
2025-04-30 12:28:46 -0300

    feature: create menu-item

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,26 +1,10 @@
 import React from "react";
+import MenuItem from "./MenuItem";
 
 function Menu() {
   return (
     <ul>
-      <li>
-        <div>
-          <picture>
-            <img src="/images/image-waffle-mobile.jpg" alt="" />
-          </picture>
-
-          <button>
-            <img src="/images/icon-add-to-cart.svg" alt="" />
-            Add to Cart
-          </button>
-        </div>
-
-        <div>
-          <h2>Waffle</h2>
-          <p>Waffle with Berries</p>
-          <span>$6.50</span>
-        </div>
-      </li>
+      <MenuItem />
     </ul>
   );
 }
