82559c7617e6f67adedaeec66e902c2db3f5baa7
Raphael Perfi <perfi.dev@proton.me>
2025-04-30 08:41:33 -0300

    chore: setup

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    rules: {
+      "no-unsed-vars": "warn",
+      "no-undef": "warn",
+    },
+  },
+  pluginReact.configs.flat.recommended,
+]);
