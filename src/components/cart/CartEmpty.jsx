cd8d3747131f1be232fe5d12de7349b4fc21c672
Raphael Perfi <perfi.dev@proton.me>
2025-05-15 12:03:05 -0300

    feat(cart): add styles to cart empty

--- src/components/cart/CartEmpty.jsx
+++ src/components/cart/CartEmpty.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 function CartEmpty() {
   return (
-    <div>
-      <h3>Your Cart (0)</h3>
+    <div className="flex flex-col gap-6 p-6 rounded-[12px] bg-white">
+      <h3 className="text-[1.5rem] font-bold leading-normal text-[#c73b07]">
+        Your Cart (0)
+      </h3>
 
-      <div>
-        <img src="/images/illustration-empty-cart.svg" alt="" />
-        <p>Your added items will appear here</p>
+      <div className="flex flex-col items-center gap-4 py-4">
+        <img src="/images/illustration-empty-cart.svg" alt="cart empty icon" />
+        <p className="text-sm font-semibold leading-[1.4] text-[#87635a]">
+          Your added items will appear here
+        </p>
       </div>
     </div>
   );
