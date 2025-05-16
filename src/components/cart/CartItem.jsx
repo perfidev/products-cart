4935fefeb5dced8cea7c8ee6ac2b53d8505f6feb
Raphael Perfi <perfi.dev@proton.me>
2025-05-16 00:25:01 -0300

    feat(cart): add styles to cart

--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -6,17 +6,26 @@ function CartItem({ item }) {
   const { dispatch } = useCart();
 
   return (
-    <li>
-      <div>
-        <span>{item.name}</span>
-        <div>
-          <span>{item.quantity}x</span>
-          <span>@ ${item.price}</span>
-          <span>${item.totalPrice}</span>
+    <li className="relative flex justify-between items-center pb-4 border-b border-[#f5eeec] last:pb-0 last:border-b-0">
+      <div className="flex flex-col gap-2">
+        <span className="text-sm font-semibold leading-normal text-[#260f08]">
+          {item.name}
+        </span>
+        <div className="flex items-center gap-2">
+          <span className="text-sm font-semibold leading-[1.4] text-[#c73b0f]">
+            {item.quantity}x
+          </span>
+          <span className="text-sm font-normal leading-[1.4] text-[#87635a]">
+            @ ${item.price}
+          </span>
+          <span className="text-sm font-semibold leading-[1.4] text-[#87635a]">
+            ${item.totalPrice}
+          </span>
         </div>
       </div>
 
       <button
+        className="flex items-center justify-center w-5 h-5 rounded-full border border-[#ad8a85] text-[#ad8a85] bg-transparent"
         onClick={() => dispatch({ type: "cart/delete", payload: item.id })}
       >
         <img src="/images/icon-remove-item.svg" alt="" />
